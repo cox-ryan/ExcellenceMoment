d2e17b503eb367468deff112b02ac964ab05760e
cox-ryan <Ryan.Cox@arup.com>
2022-12-05 15:50:26 -0500

    style of home page, workng on recording page

--- application.js
+++ application.js
@@ -7,23 +7,16 @@ const canvas = document.querySelector('.visualizer');
 const mainSection = document.querySelector('.main-controls');
 
 
-const modal = document.querySelector(".modal");
+
 const trigger = document.querySelector(".trigger");
 const closeButton = document.querySelector(".close-button");
 
-function toggleModal() {
-    modal.classList.toggle("show-modal");
-}
+
 
 function windowOnClick(event) {
     if (event.target === modal) {
-        toggleModal();
     }
 }
-
-
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
 // disable stop button while not recording
