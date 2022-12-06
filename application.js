103ad4786ac51cf129910e313883987fccd20ad0
Joseph Digerness <Joseph.Digerness@arup.com>
2022-12-06 17:27:57 -0500

    change last button to Exit, added time out to go back to index

--- application.js
+++ application.js
@@ -263,6 +263,44 @@ function visualize(stream) {
   }
 }
 
+/// go back to front page after a period of inactivity
+var timeoutID;
+ 
+function setupTimer() {
+    this.addEventListener("mousemove", resetTimer, false);
+    this.addEventListener("mousedown", resetTimer, false);
+    this.addEventListener("keypress", resetTimer, false);
+    this.addEventListener("DOMMouseScroll", resetTimer, false);
+    this.addEventListener("mousewheel", resetTimer, false);
+    this.addEventListener("touchmove", resetTimer, false);
+    this.addEventListener("MSPointerMove", resetTimer, false);
+    startTimer();
+}
+setupTimer();
+ 
+function startTimer() {
+    // wait 60 seconds before calling goInactive
+    timeoutID = window.setTimeout(goInactive, 60000);
+}
+ 
+function resetTimer(e) {
+    window.clearTimeout(timeoutID);
+    goActive();
+}
+ 
+function goInactive() {
+    // do something
+    window.location.href = 'index.html';
+}
+ 
+function goActive() {
+    // do something
+    // window.location.href = 'index.html';
+    startTimer();
+}
+
+
+
 
 window.onresize = function () {
   canvas.width = mainSection.offsetWidth;
